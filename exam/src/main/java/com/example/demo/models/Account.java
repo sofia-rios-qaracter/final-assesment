564656cca19d7320bc06fccb6f458009dfab5161
Sofia <sofia.rios@qaracter.com>
2026-01-14 11:05:37 +0000

    feat: Added JPA notation to models

--- exam/src/main/java/com/example/demo/models/Account.java
+++ exam/src/main/java/com/example/demo/models/Account.java
@@ -1,19 +1,38 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+import javax.annotation.processing.Generated;
 import java.util.List;
 
+@Entity
 public class Account {
-    Long id;
-    String ownerName;
-    String iban;
-    Double balance;
-    List<Transaction> transactions;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public void deposit(Double amount){
+    @NotNull(message = "Must have an owner name")
+    @Pattern(regexp = ".*", message = "The owner name can't be empty")
+    private String ownerName;
+
+    @NotNull(message = "Must have an IBAN associated")
+    @Pattern(regexp = "^ES.*", message = "IBAN must start with ES")
+    private String iban;
+
+    @Positive(message = "Balance must be a positive")
+    private Double balance;
+
+    private List<Transaction> transactions;
+
+    public void deposit(@Valid Double amount){
         // amount > 0, add a transaction of type "deposit", update the balance
     }
 
-    public void withdraw(Double amount){
+    public void withdraw(@Valid Double amount){
         // amount > 0
         // must check that sufficient balance exists
         // Adds a transaction of type withdraw
--- exam/src/main/java/com/example/demo/models/Transaction.java
+++ exam/src/main/java/com/example/demo/models/Transaction.java
@@ -1,10 +1,24 @@
 package com.example.demo.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Pattern(regexp = "^(DEPOSIT|WITHDRAW)$", message = "Needed to be a DEPOSIT or a WITHDRAW type")
     String type;
+
+    @Positive(message = "Must be a positive ammout")
     Double amount;
     LocalDateTime date;
     // amount > 0, amount = deposit | withdraw

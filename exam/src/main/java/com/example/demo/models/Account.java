21fecb23fa3624136e52b6cc52110e2bc0aa0174
Sofia <sofia.rios@qaracter.com>
2026-01-14 10:14:36 +0000

    Feat: Created structure of Account  model

--- exam/src/main/java/com/example/demo/models/Account.java
+++ exam/src/main/java/com/example/demo/models/Account.java
@@ -1,9 +1,26 @@
 package com.example.demo.models;
 
+import java.util.List;
+
 public class Account {
     Long id;
     String ownerName;
     String iban;
     Double balance;
     List<Transaction> transactions;
+
+    public void deposit(Double amount){
+        // amount > 0, add a transaction of type "deposit", update the balance
+    }
+
+    public void withdraw(Double amount){
+        // amount > 0
+        // must check that sufficient balance exists
+        // Adds a transaction of type withdraw
+        // update balance
+    }
+
+    public List<Transaction> getTransactions(){
+        return this.transactions;
+    }
 }

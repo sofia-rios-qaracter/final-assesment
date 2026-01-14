b0e1ae95b74abfa5bf9700f133de3b05894c9c87
Sofia <sofia.rios@qaracter.com>
2026-01-14 11:45:53 +0000

    fix: Changed account not found exception parameter

--- exam/src/main/java/com/example/demo/exceptions/AccountNotFoundException.java
+++ exam/src/main/java/com/example/demo/exceptions/AccountNotFoundException.java
@@ -1,7 +1,7 @@
 package com.example.demo.exceptions;
 
 public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(String message) {
-        super(message);
+    public AccountNotFoundException(Long id) {
+        super("The account with the id: "+id+" was not found");
     }
 }

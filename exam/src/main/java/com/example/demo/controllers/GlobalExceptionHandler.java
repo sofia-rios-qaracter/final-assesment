3a5581a208434cfada74c7b45db983e630634670
Sofia <sofia.rios@qaracter.com>
2026-01-14 10:31:47 +0000

    feat: Added Global Exception Handler structure

--- exam/src/main/java/com/example/demo/controllers/GlobalExceptionHandler.java
+++ exam/src/main/java/com/example/demo/controllers/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ValidationExceptionHandler {
+public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception){

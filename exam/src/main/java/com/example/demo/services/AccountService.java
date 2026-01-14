0293af89b1602a83b2f6af1ca06f813436ff988a
Sofia <sofia.rios@qaracter.com>
2026-01-14 11:56:28 +0000

    fix: Renamed folder to DTO

--- exam/src/main/java/com/example/demo/services/AccountService.java
+++ exam/src/main/java/com/example/demo/services/AccountService.java
@@ -1,6 +1,6 @@
 package com.example.demo.services;
 
-import com.example.demo.entities.AccountRequestDTO;
+import com.example.demo.dto.AccountRequestDTO;
 import com.example.demo.exceptions.AccountNotFoundException;
 import com.example.demo.models.Account;
 import com.example.demo.models.Transaction;
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AccountService {

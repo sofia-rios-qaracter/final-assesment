6ab4540588d7222947e7a3e80e3b8d5c63f8ef2b
Sofia <sofia.rios@qaracter.com>
2026-01-14 09:03:20 +0000

    Creacion del proyecto

---
+++ exam/src/main/java/com/example/demo/controllers/ValidationExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error) ->
+            errors.put(
+                    ((FieldError) error).getField(),
+                    error.getDefaultMessage()
+            )
+        );
+
+        return errors;
+    }
+}

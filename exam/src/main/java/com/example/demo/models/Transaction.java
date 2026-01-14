253c9b38a64af14fd6644a3d078af4f1455d8f14
Sofia <sofia.rios@qaracter.com>
2026-01-14 11:15:14 +0000

    feat: Transaction model completed

--- exam/src/main/java/com/example/demo/models/Transaction.java
+++ exam/src/main/java/com/example/demo/models/Transaction.java
@@ -3,9 +3,12 @@ package com.example.demo.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import org.aspectj.lang.annotation.After;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
@@ -15,11 +18,40 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @NotNull(message = "Transactions must have a type")
     @Pattern(regexp = "^(DEPOSIT|WITHDRAW)$", message = "Needed to be a DEPOSIT or a WITHDRAW type")
     String type;
 
-    @Positive(message = "Must be a positive ammout")
+    @NotNull(message = "Transactions must have an amount")
+    @Positive(message = "Must be a positive amount")
     Double amount;
+
     LocalDateTime date;
+
     // amount > 0, amount = deposit | withdraw
+    public Transaction(@Valid String type, @Valid Double amount, LocalDateTime date){
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Transaction(@Valid String type, @Valid Double amount){
+        this(type, amount, LocalDateTime.now());
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }

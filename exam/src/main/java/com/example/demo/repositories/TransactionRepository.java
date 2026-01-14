4587071d11d2cee6bd09e060236eaa3c9f3f3f80
Sofia <sofia.rios@qaracter.com>
2026-01-14 10:48:13 +0000

    feat: Added Transaction repo

--- exam/src/main/java/com/example/demo/repositories/TransactionRepository.java
+++ exam/src/main/java/com/example/demo/repositories/TransactionRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.repositories;
 
-public interface TransactionRepository {
+import com.example.demo.models.Account;
+import com.example.demo.models.Transaction;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
 }
